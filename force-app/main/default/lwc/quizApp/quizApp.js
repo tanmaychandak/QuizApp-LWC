95efde057bc889160f81170e8cad73a4c219a0eb
tanmaychandak <tanmaychandak13@gmail.com>
2024-09-24 22:16:20 +0530

    Quiz App

---
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -0,0 +1,68 @@
+import { LightningElement } from 'lwc';
+
+export default class QuizApp extends LightningElement {
+    myQuestions=[
+        {
+            id:"Question1",
+            question:"which of the following is not a template loop",
+            answers:{
+                a:"for:each",
+                b:"iterator",
+                c:"map loop"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id:"Question2",
+            question:"which of the file is invalid in LWC component folder",
+            answers:{
+                a:".svg",
+                b:"iterator",
+                c:"map loop"
+            },
+            correctAnswer:"b"
+        },
+        {
+            id:"Question3",
+            question:"which of the following is not a directive",
+            answers:{
+                a:"for:each",
+                b:"if:true",
+                c:"@track"
+            },
+            correctAnswer:"c"
+        }
+    ]
+    selected={}
+    isSubmitted = false
+    correctAnswers = 0
+
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
+    }
+
+    get isScoredFull(){
+        return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers ? 'slds-text-color_success' : 'slds-text-color_error'}`
+    }
+
+    changeHandler(event){
+        console.log("name",event.target.name)
+        console.log("value",event.target.value)
+        const {name,value} = event.target
+
+        this.selected = {...this.selected, [name]:value}
+    }
+
+    submitHandler(event){
+        event.preventDefault()
+        let correct = this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+        this.correctAnswers = correct.length
+        this.isSubmitted = true
+    }
+
+    resetHandler(){
+        this.selected = {}
+        this.isSubmitted = false
+        this.correctAnswers = 0
+    }
+}
